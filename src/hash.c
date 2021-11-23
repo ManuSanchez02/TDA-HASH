16093f38efc5fb4a3f4074c88e9e42ecb57f41a1
Manuel Sanchez <manu.sfdlv@gmail.com>
2021-11-23 17:34:48 -0300

    Changes to add and implementation of remove, contains and obtain
    When inserting something with an existing key, it should override and free the value

--- src/hash.c
+++ src/hash.c
@@ -2,9 +2,11 @@
 #include "hash_interno.h"
 
 const int CAPACIDAD_MINIMA = 3;
-const int TAMANIO_MAXIMO_ARBOL = 10;
 const int ERROR = -1;
 const int EXITO = 0;
+const int MULTIPLICADOR_LIMITE_REHASHEO = 5;
+const int MULTIPLICADOR_NUEVO_TAMANIO = 3;
+
 
 hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inicial){
     if(capacidad_inicial < CAPACIDAD_MINIMA)
@@ -32,8 +34,26 @@ hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inic
 int hash_insertar(hash_t* hash, const char* clave, void* elemento){
     if(!hash || !clave)
         return ERROR;
-    
+
+    /*if(hash->cantidad_actual_casilleros > hash->cantidad_maxima_tabla*MULTIPLICADOR_LIMITE_REHASHEO){
+        if(!rehashear(hash))
+            return ERROR;
+    }*/ // TODO: Rehashear despues de cierta cantidad
+
     size_t clave_hasheada = hashear(clave) % hash->cantidad_maxima_tabla;
+
+    int pos_casillero = obtener_posicion_casillero(hash, clave);
+    if(pos_casillero != -1){
+        casillero_t* casillero_a_reemplazar = lista_elemento_en_posicion(hash->tabla_hash[clave_hasheada], pos_casillero);
+        if(hash->destructor)
+            hash->destructor(casillero_a_reemplazar->valor);
+
+        casillero_a_reemplazar->valor = elemento;
+        return EXITO;
+    }
+
+    
+
     casillero_t* casillero = inicializar_casillero(clave, elemento);
     if(!casillero)
         return ERROR;
@@ -47,26 +67,61 @@ int hash_insertar(hash_t* hash, const char* clave, void* elemento){
         hash->cantidad_actual_tabla++;
     }
 
+
+
     if(!lista_insertar(hash->tabla_hash[clave_hasheada], casillero))
         return ERROR;
 
     hash->cantidad_actual_casilleros++;
-    return EXITO; // TODO: Rehashear despues de cierta cantidad
+    return EXITO; 
 }
 
 
 int hash_quitar(hash_t* hash, const char* clave){
-    return 0;
+    if(!hash || !clave)
+        return ERROR;
+
+
+    size_t clave_hasheada = hashear(clave) % hash->cantidad_maxima_tabla;
+
+    int pos_casillero = obtener_posicion_casillero(hash, clave);
+    if(pos_casillero != -1){
+        casillero_t* casillero = lista_quitar_de_posicion(hash->tabla_hash[clave_hasheada], pos_casillero);
+        free(casillero->clave);
+        if(hash->destructor)
+            hash->destructor(casillero->valor);
+        free(casillero);
+        hash->cantidad_actual_casilleros--;
+        
+        return EXITO;
+    }
+
+    return ERROR;
 }
 
 
 void* hash_obtener(hash_t* hash, const char* clave){
-    return NULL;
+    if(!hash || !clave)
+        return NULL;
+
+    size_t clave_hasheada = hashear(clave) % hash->cantidad_maxima_tabla;
+    int pos_casillero = obtener_posicion_casillero(hash, clave);
+
+    if(pos_casillero == -1)
+        return NULL;
+    
+    casillero_t* casillero = lista_elemento_en_posicion(hash->tabla_hash[clave_hasheada], pos_casillero);
+    return casillero->valor;
 }
 
 
 bool hash_contiene(hash_t* hash, const char* clave){
-    return false;
+    if(!hash || !clave)
+        return false;
+
+    int pos_casillero = obtener_posicion_casillero(hash, clave);
+
+    return (pos_casillero != -1);
 }
 
 
